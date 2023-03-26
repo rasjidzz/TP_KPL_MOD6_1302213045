6a40b61a86ef0879278ebfca5b28fcf7865a5889
Muhammad Risjad Shidqi Febian <mrisjads@gmail.com>
2023-03-27 05:33:04 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TP_MODUL6_1302213045
+{
+    class Mainprogram
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Risjad Shidqi Febian");
+            video1.increaseplaycount(50);
+            video1.printvideodetails();
+            SayaTubeVideo video2 = new SayaTubeVideo("Roger Federer courtlevel Practice");
+            video2.increaseplaycount(10);
+            video2.printvideodetails();
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MODUL6_1302213045
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+        public SayaTubeVideo(String judul)
+        {
+            this.title = judul;
+            this.playcount = 0;
+            Random rand = new Random();
+            int randid = rand.Next(00000, 99999);
+            this.id = randid;
+        }
+        public void increaseplaycount(int count)
+        {
+            this.playcount += count; 
+        }
+        public void printvideodetails()
+        {
+            Console.WriteLine("\nID Film : " + this.id);
+            Console.WriteLine("Judul Film : " + this.title);
+            Console.WriteLine("Playcount Film : " + this.playcount);
+        }
+    }
+}
