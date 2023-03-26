ead4ff1e8b7ba619c789cb304e5a3249ab7011fa
Muhammad Risjad Shidqi Febian <mrisjads@gmail.com>
2023-03-27 05:51:15 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@
             SayaTubeVideo video2 = new SayaTubeVideo("Roger Federer courtlevel Practice");
             video2.increaseplaycount(10);
             video2.printvideodetails();
+            SayaTubeVideo video3 = new SayaTubeVideo("This is an example title that is exactly 100 characters long and should not cause an exception to be thrown");
+            video3.increaseplaycount(10000001);
+            video3 .printvideodetails();
         }
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,10 @@ namespace TP_MODUL6_1302213045
         private int playcount;
         public SayaTubeVideo(String judul)
         {
-            this.title = judul;
+            if (judul !=  null || judul.Length <100)
+            {
+                this.title = judul;
+            }
             this.playcount = 0;
             Random rand = new Random();
             int randid = rand.Next(00000, 99999);
@@ -21,7 +25,25 @@ namespace TP_MODUL6_1302213045
         }
         public void increaseplaycount(int count)
         {
-            this.playcount += count; 
+            if (count > 0 || count < 10000000)
+            {
+                for(int i = 0; i < count;)
+                {
+                    this.playcount++;
+                    i++;
+                }
+                int coba = 0;
+                try
+                {
+                    coba = checked(count + 10);
+                }
+                catch (System.OverflowException b)
+                {
+                    Console.WriteLine("Cek : " + b.ToString);
+                    Console.WriteLine("Count melebihi 10000000");
+                }
+            }
+            
         }
         public void printvideodetails()
         {
